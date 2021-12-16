c6b3e0a2908e876c454eee4e8a233625d4783063
Sebastian <sebapilch@student.agh.edu.pl>
2021-12-16 18:10:45 +0100

    'cos'

---
+++ cmake-build-debug/CMakeFiles/sieci.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sieci.dir/main.cpp.obj"
+  "libsieci.dll.a"
+  "sieci.exe"
+  "sieci.exe.manifest"
+  "sieci.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sieci.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/storage.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Sebastian on 16.12.2021.
+//
+
+#ifndef SIECI_STORAGE_HPP
+#define SIECI_STORAGE_HPP
+
+class Package
+
+class PackageQueue
+
+class IPackageQueue
+
+class IPackageStockpile
+
+
+#endif //SIECI_STORAGE_HPP
